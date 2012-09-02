91d65ee99960fec207425b6bf068cdaa3f93f8b6
Jason Thrasher <jthrasher@brightroll.com>
2012-09-01 21:34:39 -0700

    Revert "files not used"
    This reverts commit 7b77eefacd22d8aac28d1f689580bd5c448fc2a6.

---
+++ src/main/webapp/calendar_schedule.js
@@ -0,0 +1,571 @@
+var mission = {
+  "result": {
+   "kind": "calendar#events",
+   "etag": "\"y3Ec45PWr056tgI9oyp5WeT3BzE/SepfWv2UK28gz6bq2NOv4h62HM0\"",
+   "summary": "SF: Mission (8)",
+   "updated": "2012-08-15T20:09:18.000Z",
+   "timeZone": "America/Los_Angeles",
+   "accessRole": "reader",
+   "items": [
+    {
+     "kind": "calendar#event",
+     "etag": "\"y3Ec45PWr056tgI9oyp5WeT3BzE/Q09EVTdMeVNKeEVBQUFBQUFBQUFBQT09\"",
+     "id": "5mu0tigt05g9bhc35al0toiaf4",
+     "status": "confirmed",
+     "htmlLink":
+"https://www.google.com/calendar/event?eid=NW11MHRpZ3QwNWc5YmhjMzVhbDB0b2lhZjQgYnJpZ2h0cm9sbC5jb21fMmQzNDMwMzQzOTMwMzUzMDM4MzczNDMyQHJlc291cmNlLmNhbGVuZGFyLmdvb2dsZS5jb20",
+     "created": "2012-07-31T20:16:52.000Z",
+     "updated": "2012-08-15T00:07:24.000Z",
+     "summary": "UK Sales Summit Agenda",
+     "description": "Call to discuss the agenda for the sales summit as well as any outstanding items for this event. If we have time we can also discuss any other outstanding ad:tech mobile summit items on the list as this will replace our weekly call.\n\n- agenda\n- logo, powerpoint layout, signage\n- transportations and meals, entertainment\n\nOther\n- ad:tech London party\n- AOP Press Relase",
+     "location": "SF:  Mission (10)",
+     "creator": {
+      "email": "samantha@brightroll.com",
+      "displayName": "Samantha Frampton"
+     },
+     "organizer": {
+      "email": "samantha@brightroll.com",
+      "displayName": "Samantha Frampton"
+     },
+     "start": {
+      "dateTime": "2012-08-15T09:00:00-07:00"
+     },
+     "end": {
+      "dateTime": "2012-08-15T10:00:00-07:00"
+     },
+     "iCalUID": "5mu0tigt05g9bhc35al0toiaf4@google.com",
+     "sequence": 0,
+     "attendees": [
+      {
+       "email": "samantha@brightroll.com",
+       "displayName": "Samantha Frampton",
+       "organizer": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "christine@brightroll.com",
+       "displayName": "Christine Schoultz",
+       "responseStatus": "accepted"
+      },
+      {
+       "email":
+"brightroll.com_2d3430343930353038373432@resource.calendar.google.com",
+       "displayName": "SF:  Mission (10)",
+       "self": true,
+       "resource": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "gina@brightroll.com",
+       "displayName": "Gina Yrrizarry",
+       "responseStatus": "needsAction"
+      },
+      {
+       "email": "phil@brightroll.com",
+       "displayName": "Phil Cooper",
+       "responseStatus": "needsAction"
+      },
+      {
+       "email": "dmcnutt@brightroll.com",
+       "displayName": "Daryl McNutt",
+       "responseStatus": "accepted"
+      }
+     ],
+     "guestsCanInviteOthers": true,
+     "guestsCanSeeOtherGuests": true,
+     "reminders": {
+      "useDefault": true
+     }
+    },
+    {
+     "kind": "calendar#event",
+     "etag": "\"y3Ec45PWr056tgI9oyp5WeT3BzE/Q09pLXNMdVNKeEVBQUFBQUFBQUFBQT09\"",
+     "id": "onm3v2jnm55jr8ikc1le1i01ds",
+     "status": "confirmed",
+     "htmlLink":
+"https://www.google.com/calendar/event?eid=b25tM3Yyam5tNTVqcjhpa2MxbGUxaTAxZHMgYnJpZ2h0cm9sbC5jb21fMmQzNDMwMzQzOTMwMzUzMDM4MzczNDMyQHJlc291cmNlLmNhbGVuZGFyLmdvb2dsZS5jb20",
+     "created": "2012-08-08T04:22:43.000Z",
+     "updated": "2012-08-14T23:16:01.000Z",
+     "summary": "BR UK Agency Survey Kick Off Call",
+     "location": "SF:  Mission (10)",
+     "creator": {
+      "email": "samantha@brightroll.com",
+      "displayName": "Samantha Frampton"
+     },
+     "organizer": {
+      "email": "samantha@brightroll.com",
+      "displayName": "Samantha Frampton"
+     },
+     "start": {
+      "dateTime": "2012-08-15T10:00:00-07:00"
+     },
+     "end": {
+      "dateTime": "2012-08-15T10:30:00-07:00"
+     },
+     "iCalUID": "onm3v2jnm55jr8ikc1le1i01ds@google.com",
+     "sequence": 1,
+     "attendees": [
+      {
+       "email": "koushalya@brightroll.com",
+       "displayName": "Koushalya Subramanian",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "samantha@brightroll.com",
+       "displayName": "Samantha Frampton",
+       "organizer": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email":
+"brightroll.com_2d3430343930353038373432@resource.calendar.google.com",
+       "displayName": "SF:  Mission (10)",
+       "self": true,
+       "resource": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "christine@brightroll.com",
+       "displayName": "Christine Schoultz",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "gina@brightroll.com",
+       "displayName": "Gina Yrrizarry",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "phil@brightroll.com",
+       "displayName": "Phil Cooper",
+       "responseStatus": "accepted"
+      }
+     ],
+     "guestsCanInviteOthers": true,
+     "guestsCanSeeOtherGuests": true,
+     "reminders": {
+      "useDefault": true
+     }
+    },
+    {
+     "kind": "calendar#event",
+     "etag": "\"y3Ec45PWr056tgI9oyp5WeT3BzE/Q0ppcjZyYVNKeEVBQUFBQUFBQUFBQT09\"",
+     "id": "rjk014oiqnh1qcnnde3bqsb6qc",
+     "status": "confirmed",
+     "htmlLink":
+"https://www.google.com/calendar/event?eid=cmprMDE0b2lxbmgxcWNubmRlM2Jxc2I2cWMgYnJpZ2h0cm9sbC5jb21fMmQzNDMwMzQzOTMwMzUzMDM4MzczNDMyQHJlc291cmNlLmNhbGVuZGFyLmdvb2dsZS5jb20",
+     "created": "2012-08-14T00:32:22.000Z",
+     "updated": "2012-08-14T20:37:03.000Z",
+     "summary": "vCE vs Classic Campaign Essentials (comScore)",
+     "location": "SF:  Mission (10)",
+     "creator": {
+      "email": "koushalya@brightroll.com",
+      "displayName": "Koushalya Subramanian"
+     },
+     "organizer": {
+      "email": "koushalya@brightroll.com",
+      "displayName": "Koushalya Subramanian"
+     },
+     "start": {
+      "dateTime": "2012-08-15T10:30:00-07:00"
+     },
+     "end": {
+      "dateTime": "2012-08-15T11:00:00-07:00"
+     },
+     "iCalUID": "rjk014oiqnh1qcnnde3bqsb6qc@google.com",
+     "sequence": 0,
+     "attendees": [
+      {
+       "email": "koushalya@brightroll.com",
+       "displayName": "Koushalya Subramanian",
+       "organizer": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email":
+"brightroll.com_2d3430343930353038373432@resource.calendar.google.com",
+       "displayName": "SF:  Mission (10)",
+       "self": true,
+       "resource": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "kjelinek@brightroll.com",
+       "displayName": "Katie Jelinek",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "markmuleady@brightroll.com",
+       "displayName": "Mark Muleady",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "ej@brightroll.com",
+       "displayName": "Emily-Jane Peters",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "clark@brightroll.com",
+       "displayName": "Clark Mackintosh",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "alyssa@brightroll.com",
+       "displayName": "Alyssa Gagliani",
+       "responseStatus": "accepted"
+      }
+     ],
+     "guestsCanInviteOthers": true,
+     "guestsCanSeeOtherGuests": true,
+     "reminders": {
+      "useDefault": true
+     }
+    },
+    {
+     "kind": "calendar#event",
+     "etag": "\"y3Ec45PWr056tgI9oyp5WeT3BzE/Q01DRDB1eVFKeEVBQUFBQUFBQUFBQT09\"",
+     "id": "b6c6d74ovvd8l8efh9a09jfk28_20120808T170000Z",
+     "status": "confirmed",
+     "htmlLink":
+"https://www.google.com/calendar/event?eid=YjZjNmQ3NG92dmQ4bDhlZmg5YTA5amZrMjhfMjAxMjA4MDhUMTcwMDAwWiBicmlnaHRyb2xsLmNvbV8yZDM0MzAzNDM5MzAzNTMwMzgzNzM0MzJAcmVzb3VyY2UuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+     "created": "2012-06-20T13:14:48.000Z",
+     "updated": "2012-08-09T22:50:00.000Z",
+     "summary": "Bi-Weekly BRX Marketing",
+     "description": "Bi-weekly meeting to:\nTouch base on current BRX marketing projects, priorities and delivery dates\nDiscuss and evaluate any new marketing opportunities the team may be hearing from the field\n\nhttps://docs.google.com/document/d/1nbietbE4pmeVPki71PSJsJF5ymrWQ_UafoAysqFXF2w/edit\n\niChat: \nlanaeweirbr",
+     "location": "Marketing 1 # : 415-634-4812, PIN : 8365, SF:  Mission (10)",
+     "creator": {
+      "email": "lanae@brightroll.com",
+      "displayName": "Lanae Weir"
+     },
+     "organizer": {
+      "email": "lanae@brightroll.com",
+      "displayName": "Lanae Weir"
+     },
+     "start": {
+      "dateTime": "2012-08-15T11:00:00-07:00"
+     },
+     "end": {
+      "dateTime": "2012-08-15T12:00:00-07:00"
+     },
+     "recurringEventId": "b6c6d74ovvd8l8efh9a09jfk28",
+     "originalStartTime": {
+      "dateTime": "2012-08-08T10:00:00-07:00"
+     },
+     "iCalUID": "b6c6d74ovvd8l8efh9a09jfk28@google.com",
+     "sequence": 4,
+     "attendees": [
+      {
+       "email": "lanae@brightroll.com",
+       "displayName": "Lanae Weir",
+       "organizer": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email":
+"brightroll.com_33373933343836392d343934@resource.calendar.google.com",
+       "displayName": "Marketing 1 # : 415-634-4812, PIN : 8365",
+       "resource": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "craig@brightroll.com",
+       "displayName": "Craig Whitmer",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "christine@brightroll.com",
+       "displayName": "Christine Schoultz",
+       "responseStatus": "needsAction"
+      },
+      {
+       "email": "dmcnutt@brightroll.com",
+       "displayName": "Daryl McNutt",
+       "optional": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email":
+"brightroll.com_2d3430343930353038373432@resource.calendar.google.com",
+       "displayName": "SF:  Mission (10)",
+       "self": true,
+       "resource": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "dmosher@brightroll.com",
+       "displayName": "Dan Mosher",
+       "responseStatus": "accepted"
+      }
+     ],
+     "reminders": {
+      "useDefault": true
+     }
+    },
+    {
+     "kind": "calendar#event",
+     "etag": "\"y3Ec45PWr056tgI9oyp5WeT3BzE/Q05qUXZOYVNKeEVBQUFBQUFBQUFBQT09\"",
+     "id": "h712be10au2morq61f5lalpafk",
+     "status": "confirmed",
+     "htmlLink":
+"https://www.google.com/calendar/event?eid=aDcxMmJlMTBhdTJtb3JxNjFmNWxhbHBhZmsgYnJpZ2h0cm9sbC5jb21fMmQzNDMwMzQzOTMwMzUzMDM4MzczNDMyQHJlc291cmNlLmNhbGVuZGFyLmdvb2dsZS5jb20",
+     "created": "2012-08-13T16:43:17.000Z",
+     "updated": "2012-08-15T15:03:03.000Z",
+     "summary": "vCE training ",
+     "location": "SF:  Mission (10) 218.486.1611 x111771#",
+     "creator": {
+      "email": "sable@brightroll.com",
+      "displayName": "Sable Mi"
+     },
+     "organizer": {
+      "email": "sable@brightroll.com",
+      "displayName": "Sable Mi"
+     },
+     "start": {
+      "dateTime": "2012-08-15T12:30:00-07:00"
+     },
+     "end": {
+      "dateTime": "2012-08-15T13:00:00-07:00"
+     },
+     "iCalUID": "h712be10au2morq61f5lalpafk@google.com",
+     "sequence": 0,
+     "attendees": [
+      {
+       "email": "nmckeith@brightroll.com",
+       "displayName": "Nicole McKeith",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "julie@brightroll.com",
+       "displayName": "Julie Flis",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "sable@brightroll.com",
+       "displayName": "Sable Mi",
+       "organizer": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "jeff@brightroll.com",
+       "displayName": "Jeff Kelosky",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "michelle@brightroll.com",
+       "displayName": "Michelle Quicho",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "bleung@brightroll.com",
+       "displayName": "Brian Leung",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "beth@brightroll.com",
+       "displayName": "Beth Richard",
+       "responseStatus": "accepted"
+      },
+      {
+       "email":
+"brightroll.com_2d3430343930353038373432@resource.calendar.google.com",
+       "displayName": "SF:  Mission (10)",
+       "self": true,
+       "resource": true,
+       "responseStatus": "accepted"
+      }
+     ],
+     "guestsCanInviteOthers": true,
+     "guestsCanSeeOtherGuests": true,
+     "reminders": {
+      "useDefault": true
+     }
+    },
+    {
+     "kind": "calendar#event",
+     "etag": "\"y3Ec45PWr056tgI9oyp5WeT3BzE/Q0xpM2ticVNKeEVBQUFBQUFBQUFBQT09\"",
+     "id": "fbgt0rlm02jpika0tbrae1nvik_20120815T203000Z",
+     "status": "confirmed",
+     "htmlLink":
+"https://www.google.com/calendar/event?eid=ZmJndDBybG0wMmpwaWthMHRicmFlMW52aWtfMjAxMjA4MTVUMjAzMDAwWiBicmlnaHRyb2xsLmNvbV8yZDM0MzAzNDM5MzAzNTMwMzgzNzM0MzJAcmVzb3VyY2UuY2FsZW5kYXIuZ29vZ2xlLmNvbQ",
+     "created": "2011-09-07T18:00:56.000Z",
+     "updated": "2012-08-14T22:32:35.000Z",
+     "summary": "Sascha/Craig 1x1",
+     "location": "SF:  Mission (10), SF: SOMA (8)",
+     "creator": {
+      "email": "sascha@brightroll.com",
+      "displayName": "Sascha Wohn"
+     },
+     "organizer": {
+      "email": "sascha@brightroll.com",
+      "displayName": "Sascha Wohn"
+     },
+     "start": {
+      "dateTime": "2012-08-15T14:30:00-07:00"
+     },
+     "end": {
+      "dateTime": "2012-08-15T15:00:00-07:00"
+     },
+     "recurringEventId": "fbgt0rlm02jpika0tbrae1nvik",
+     "originalStartTime": {
+      "dateTime": "2012-08-15T13:30:00-07:00",
+      "timeZone": "America/Los_Angeles"
+     },
+     "iCalUID": "fbgt0rlm02jpika0tbrae1nvik@google.com",
+     "sequence": 2,
+     "attendees": [
+      {
+       "email":
+"brightroll.com_2d38333031373532372d333831@resource.calendar.google.com",
+       "displayName": "SF: SOMA (8)",
+       "resource": true,
+       "responseStatus": "declined"
+      },
+      {
+       "email": "craig@brightroll.com",
+       "displayName": "Craig Whitmer",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "sascha@brightroll.com",
+       "displayName": "Sascha Wohn",
+       "organizer": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email":
+"brightroll.com_2d3430343930353038373432@resource.calendar.google.com",
+       "displayName": "SF:  Mission (10)",
+       "self": true,
+       "resource": true,
+       "responseStatus": "accepted"
+      }
+     ],
+     "guestsCanInviteOthers": true,
+     "guestsCanModify": true,
+     "guestsCanSeeOtherGuests": true,
+     "reminders": {
+      "useDefault": true
+     }
+    },
+    {
+     "kind": "calendar#event",
+     "etag": "\"y3Ec45PWr056tgI9oyp5WeT3BzE/Q1BEM3lMR1NKeEVBQUFBQUFBQUFBQT09\"",
+     "id": "07er4buom5c5pn7gv6efg1af1o",
+     "status": "confirmed",
+     "htmlLink":
+"https://www.google.com/calendar/event?eid=MDdlcjRidW9tNWM1cG43Z3Y2ZWZnMWFmMW8gYnJpZ2h0cm9sbC5jb21fMmQzNDMwMzQzOTMwMzUzMDM4MzczNDMyQHJlc291cmNlLmNhbGVuZGFyLmdvb2dsZS5jb20",
+     "created": "2012-08-14T17:09:42.000Z",
+     "updated": "2012-08-14T17:33:10.000Z",
+     "summary": "Marketing Discussion",
+     "location": "SF:  Mission (10)",
+     "creator": {
+      "email": "Gina@brightroll.com",
+      "displayName": "Gina Yrrizarry"
+     },
+     "organizer": {
+      "email": "Gina@brightroll.com",
+      "displayName": "Gina Yrrizarry"
+     },
+     "start": {
+      "dateTime": "2012-08-15T15:00:00-07:00"
+     },
+     "end": {
+      "dateTime": "2012-08-15T15:30:00-07:00"
+     },
+     "iCalUID": "07er4buom5c5pn7gv6efg1af1o@google.com",
+     "sequence": 0,
+     "attendees": [
+      {
+       "email": "valarie@brightroll.com",
+       "displayName": "Valarie Arismendez",
+       "responseStatus": "accepted"
+      },
+      {
+       "email":
+"brightroll.com_2d3430343930353038373432@resource.calendar.google.com",
+       "displayName": "SF:  Mission (10)",
+       "self": true,
+       "resource": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "christine@brightroll.com",
+       "displayName": "Christine Schoultz",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "gina@brightroll.com",
+       "displayName": "Gina Yrrizarry",
+       "organizer": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "dmcnutt@brightroll.com",
+       "displayName": "Daryl McNutt",
+       "responseStatus": "accepted"
+      }
+     ],
+     "guestsCanInviteOthers": true,
+     "guestsCanSeeOtherGuests": true,
+     "reminders": {
+      "useDefault": true
+     }
+    },
+    {
+     "kind": "calendar#event",
+     "etag": "\"y3Ec45PWr056tgI9oyp5WeT3BzE/Q01qVW9QaVFKeEVBQUFBQUFBQUFBQT09\"",
+     "id": "59ib2rrdh5eot66nterolq0g9s",
+     "status": "confirmed",
+     "htmlLink":
+"https://www.google.com/calendar/event?eid=NTlpYjJycmRoNWVvdDY2bnRlcm9scTBnOXMgYnJpZ2h0cm9sbC5jb21fMmQzNDMwMzQzOTMwMzUzMDM4MzczNDMyQHJlc291cmNlLmNhbGVuZGFyLmdvb2dsZS5jb20",
+     "created": "2012-08-09T22:02:17.000Z",
+     "updated": "2012-08-10T05:35:57.000Z",
+     "summary": "DS 1x1 (in SF)",
+     "description": "Thought to replace this w/ our Friday touch base since we'll both be in SF.",
+     "location": "SF:  Mission (10)",
+     "creator": {
+      "email": "sabrina@brightroll.com",
+      "displayName": "Sabrina Ho"
+     },
+     "organizer": {
+      "email": "sabrina@brightroll.com",
+      "displayName": "Sabrina Ho"
+     },
+     "start": {
+      "dateTime": "2012-08-15T16:00:00-07:00"
+     },
+     "end": {
+      "dateTime": "2012-08-15T22:00:00-07:00"
+     },
+     "iCalUID": "59ib2rrdh5eot66nterolq0g9s@google.com",
+     "sequence": 0,
+     "attendees": [
+      {
+       "email":
+"brightroll.com_2d3430343930353038373432@resource.calendar.google.com",
+       "displayName": "SF:  Mission (10)",
+       "self": true,
+       "resource": true,
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "dmcnutt@brightroll.com",
+       "displayName": "Daryl McNutt",
+       "responseStatus": "accepted"
+      },
+      {
+       "email": "sabrina@brightroll.com",
+       "displayName": "Sabrina Ho",
+       "organizer": true,
+       "responseStatus": "accepted"
+      }
+     ],
+     "guestsCanInviteOthers": true,
+     "guestsCanSeeOtherGuests": true,
+     "reminders": {
+      "useDefault": true
+     }
+    }
+   ]
+  },
+  "id": "gapiRpc"
+ };
