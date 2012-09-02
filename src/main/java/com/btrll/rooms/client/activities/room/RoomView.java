1bda5b928307e08bae618bfd99d376f157d8bdde
Jason Thrasher <jthrasher@brightroll.com>
2012-09-01 21:46:11 -0700

    Now using UiBinder for RoomView UI.

--- src/main/java/com/btrll/rooms/client/activities/room/RoomView.java
+++ src/main/java/com/btrll/rooms/client/activities/room/RoomView.java
@@ -1,55 +1,31 @@
 package com.btrll.rooms.client.activities.room;
 
 import com.btrll.rooms.client.DetailView;
-import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.Label;
-import com.googlecode.mgwt.ui.client.widget.ProgressBar;
+import com.google.gwt.user.client.ui.Widget;
+import com.googlecode.mgwt.ui.client.widget.ProgressIndicator;
 
 public class RoomView extends DetailView implements RoomActivity.View {
-	// private static final Binder binder = GWT.create(Binder.class);
-	//
-	// interface Binder extends UiBinder<Widget, RoomView> {
-	// }
-	//
-	// @UiField
-	// FlowPanel flow;
+	private static final Binder binder = GWT.create(Binder.class);
+
+	interface Binder extends UiBinder<Widget, RoomView> {
+	}
+
+	@UiField
+	FlowPanel panel;
+	@UiField
+	ProgressIndicator pi;
 
 	public RoomView() {
+		initWidget(binder.createAndBindUi(this));
 
-		// initWidget(binder.createAndBindUi(this));
-
-		FlowPanel content = new FlowPanel();
-		content.getElement().getStyle().setMarginTop(20, Unit.PX);
-
-		content.add(new ProgressBar());
-
-//		HTML html = new HTML("animation is purely done with css");
-//		html.getElement().setAttribute("style", "text-align: center; padding: 20px;");
-//		content.add(html);
-		
-		Label status = new Label("status");
-		Image image = new Image("images/busy.png");
-		
-		content.add(status);
-		content.add(image);
-		status.getElement().setId("availability-description");
-		image.getElement().setId("availability-image");
-
-
-		scrollPanel.setWidget(content);
+		scrollPanel.setWidget(panel);
 	}
 
 	@Override
 	public void setRoom(String svg) {
-//		panel.removeFromParent();
-//
-//		panel = new LayoutPanel();
-//		panel.getElement().setId("map");
-//		pullToRefresh.add(panel);
-//
-//		inject(svg);
 	}
 }
