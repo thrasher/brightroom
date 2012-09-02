c3204ff9360e4cde5206cfa0a883e404f8896eed
Jason Thrasher <jthrasher@brightroll.com>
2012-09-02 00:52:37 -0700

    Tweaking slide animations between places on phones.

--- src/main/java/com/btrll/rooms/client/PhoneAnimationMapper.java
+++ src/main/java/com/btrll/rooms/client/PhoneAnimationMapper.java
@@ -2,6 +2,8 @@ package com.btrll.rooms.client;
 
 import com.btrll.rooms.client.activities.AboutPlace;
 import com.btrll.rooms.client.activities.RoomListPlace;
+import com.btrll.rooms.client.activities.map.MapPlace;
+import com.btrll.rooms.client.activities.room.RoomPlace;
 import com.btrll.rooms.client.places.HomePlace;
 import com.google.gwt.place.shared.Place;
 import com.googlecode.mgwt.mvp.client.Animation;
@@ -12,21 +14,28 @@ public class PhoneAnimationMapper implements AnimationMapper {
 	@Override
 	public Animation getAnimation(Place oldPlace, Place newPlace) {
 
+		if (oldPlace instanceof HomePlace && newPlace instanceof RoomListPlace) {
+			return Animation.SLIDE;
+		}
 		if (oldPlace instanceof RoomListPlace && newPlace instanceof HomePlace) {
 			return Animation.SLIDE_REVERSE;
 		}
-
+		if (oldPlace instanceof RoomListPlace && newPlace instanceof RoomPlace) {
+			return Animation.SLIDE;
+		}
+		if (oldPlace instanceof RoomPlace && newPlace instanceof RoomListPlace) {
+			return Animation.SLIDE_REVERSE;
+		}
+		if (oldPlace instanceof MapPlace && newPlace instanceof RoomListPlace) {
+			return Animation.SLIDE_REVERSE;
+		}
+		if (oldPlace instanceof HomePlace && newPlace instanceof AboutPlace) {
+			return Animation.SLIDE_UP;
+		}
 		if (oldPlace instanceof AboutPlace && newPlace instanceof HomePlace) {
 			return Animation.SLIDE_UP_REVERSE;
 		}
 
-		if (oldPlace instanceof HomePlace && newPlace instanceof AboutPlace) {
-			return Animation.SLIDE_UP;
-		}
-
-		if (oldPlace instanceof HomePlace && newPlace instanceof RoomListPlace) {
-			return Animation.SLIDE;
-		}
 
 		// default animation
 
